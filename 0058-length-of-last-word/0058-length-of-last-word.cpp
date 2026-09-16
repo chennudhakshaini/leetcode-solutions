f2eb21005a3d78abe742dfcf01815d88fda2e7ba
Dhakshaini Chennu <chennudhakshaini@gmail.com>
2026-09-16 20:30:58 +0530

    Time: 0 ms (100%), Space: 8.7 MB (92.39%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.length(),cnt=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[i]!=' ')
+            cnt++;
+            else if(cnt>0)
+            break;
+        }
+        return cnt;
+    }
+};
